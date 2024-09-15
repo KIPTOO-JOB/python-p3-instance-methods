80ab610cb40f34f8e3d24892d6c1438ff49e6b5d
KIPTOO-JOB <job3kiptoo@gmail.com>
2024-09-15 17:46:02 +0300

    Finished  the lab

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,15 @@ class Person:
     # Class body goes here
 
     #Instance method definition
+    def talk(self):
+        return("Hello World!")
+        
+    def walk(self):
+       return("The person is walking.")
+    
     pass
+Job = Person()
+
+
+print(Job.walk())
+print(Job.talk())
